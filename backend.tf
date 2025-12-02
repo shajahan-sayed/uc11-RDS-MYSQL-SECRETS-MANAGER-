b245e580109afd1fbcab5b27e4174d266ad95b1c
shajahan-sayed <shajahansayed111@gmail.com>
2025-12-02 23:18:34 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
    backend "s3" {
-       bucket = "two_tier_backend"
+       bucket = "two-tier-backend"
        key = "terraform.tfstate"
-       dynamodb_table = "two_tier_backend"
        region = "ap-southeast-2"
     }
 }
